c3fa3ec93156829f3610fe642db736cfd53906d2
AbdelRahman Elrefai <team.man00@gmail.com>
2026-02-10 05:31:24 +0200

    C: Add repository interfaces (IBookRepository, IAuthorRepository, IUnitOfWork) to Domain layer

---
+++ DigitalLibrary.Domain/Repositories/IAuthorRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DigitalLibrary.Domain.Entities;
+using DigitalLibrary.Domain.ValueObjects;
+
+namespace DigitalLibrary.Domain.Repositories
+{
+    public interface IAuthorRepository
+    {
+        // Query Operations
+        Task<Author?> GetByIdAsync(AuthorId id, CancellationToken cancellationToken = default);
+        Task<Author?> GetByNameAsynce(AuthorId id, CancellationToken cancellationToken = default);
+        Task<IReadOnlyCollection<Author>> GetAll(CancellationToken cancellationToken = default);
+
+        // Command Operations
+        Task AddAuthorAsync(Author author, CancellationToken cancellationToken = default);
+        Task Remove(Author author);
+    }
+}
---
+++ DigitalLibrary.Domain/Repositories/IBookRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DigitalLibrary.Domain.Entities;
+using DigitalLibrary.Domain.ValueObjects;
+
+namespace DigitalLibrary.Domain.Repositories
+{
+    public interface IBookRepository
+    {
+        // Read operations
+        Task<Book?> GetByIdAsync(BookId bookId, CancellationToken cancellationToken = default);
+        Task<Book?> GetByIsbnAsync(string isbn,CancellationToken cancellationToken = default);
+        Task<IReadOnlyCollection<Book>> GetAvailableBooksAsync(CancellationToken cancellationToken = default);
+        Task<IReadOnlyCollection<Book>> GetByAuthorAsync(AuthorId authorId, CancellationToken cancellationToken = default);
+
+        // Write Operations
+        // No Update.. Because you can get the book by get by id, EF Core would track the returned object, and you can update it then.
+        Task AddAsync(Book book, CancellationToken cancellationToken = default);
+        void Remove(Book book);
+
+    }
+}
